b6e29e75dc88f886427f6e3a6d9a11d54da2870b
Gaughar2 <35746305+Gaughar2@users.noreply.github.com>
2018-05-08 09:10:57 +0100

    Update deploy_demo_test2.R

--- deploy_demo_test2.R
+++ deploy_demo_test2.R
@@ -5,7 +5,7 @@ load("PredictionDemo.RData")
 #* @post /predict
 predict <- function(dob,height,weight,diabetes,bloodSugar,historicAdverseTerms,highBloodPressure,aviation,cholesterolReading){
     data <-list(dob=dob,height=height,weight=weight,diabetes=diabetes,bloodSugar=bloodSugar,historicAdverseTerms=historicAdverseTerms,highBloodPressure =highBloodPressure,aviation=aviation,cholesterolReading=cholesterolReading)
-    prediction <- do.call(Demo,data)
+    prediction <- unbox(do.call(Demo,data))
  
 }
 
